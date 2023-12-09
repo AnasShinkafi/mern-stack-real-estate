6550f90415b0c91868fd025d321a8dd753158574
Anas Isah <anasisahskf59@gmail.com>
2023-12-09 20:35:30 +0100

    fix error

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: import.meta.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-f684a.firebaseapp.com",
+  projectId: "mern-estate-f684a",
+  storageBucket: "mern-estate-f684a.appspot.com",
+  messagingSenderId: "155968047837",
+  appId: "1:155968047837:web:37a126ea9f7594d1fd2934"
 };
 
 // Initialize Firebase
